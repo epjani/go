92788c39be41ea979f9f67b4506883f903699fcf
epjani <edin.pjanic@atlantbh.com>
2013-05-18 14:03:06 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ginekologija::Application.config.secret_token = 'd95918b9c791910a2b0b7d1fb7a2c5422ea58347c0a23ba53a36aed7d41031e9421dba426eafccaafd77dff1ab0d3fea9cff909739f6332ccb8f8d724ced01e5'
