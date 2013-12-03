89b3f4ee8f4da0c5a8563851f008ef8bbbd2e609
epjani <edin.pjanic@atlantbh.com>
2013-12-03 23:11:33 +0100

    change date display

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,11 +2,12 @@ class DashboardsController < ApplicationController
 
 	def index
 		@doctors = @nurse.get_doctors
-		@selected_date = params[:selected_date].blank? ? DateTime.now.strftime("%Y/%m/%d") : params[:selected_date].to_date
+		sel_date = params[:selected_date].blank? ? DateTime.now : params[:selected_date].to_date
+		@selected_date = sel_date.strftime("%d.%m.%Y")
 		@doctor_id = params[:doctor_id].blank? ? (@doctors.empty? ? nil : @doctors.first.id) : params[:doctor_id]
 		@doctor_name = Doctor.find(@doctor_id).full_name if @doctor_id
 		@shift = params[:shift].blank? ? 1 : params[:shift].to_i
-		@reservations = @doctors.empty? ? [] : @nurse.get_reservations(@selected_date, @doctor_id, @shift)
+		@reservations = @doctors.empty? ? [] : @nurse.get_reservations(sel_date, @doctor_id, @shift)
 		
 		puts @reservations.inspect
 	end
--- db/migrate/20130718203401_add_shift_to_examination_time.rb
+++ db/migrate/20130718203401_add_shift_to_examination_time.rb
@@ -1,9 +1,11 @@
 class AddShiftToExaminationTime < ActiveRecord::Migration
   def up
   	add_column :examination_times, :shift, :integer
+  	add_column :examination_times, :et_index, :integer
   end
 
   def down
   	remove_column :examination_times, :shift
+  	remove_column :examination_times, :et_index
   end
 end
